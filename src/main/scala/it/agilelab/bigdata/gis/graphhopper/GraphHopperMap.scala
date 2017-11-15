49d80c32c7f21f05bd491af436faa12170326087
Stefano_Samele <stefano.samele@agilelab.it>
2017-11-15 18:36:47 +0100

    GraphHopperMap to singleton

--- src/main/scala/it/agilelab/bigdata/gis/graphhopper/GraphHopperMap.scala
+++ src/main/scala/it/agilelab/bigdata/gis/graphhopper/GraphHopperMap.scala
@@ -1,81 +1,66 @@
 package it.agilelab.bigdata.gis.graphhopper
 
-import java.util
 import java.util.Locale
 
 import com.graphhopper.{GHRequest, GHResponse, PathWrapper}
-import com.graphhopper.matching.{EdgeMatch, GPXExtension, MapMatching, MatchResult}
+import com.graphhopper.matching.{EdgeMatch, MapMatching, MatchResult}
+
 
 import scalax.file.Path
 import com.graphhopper.reader.osm.GraphHopperOSM
 import com.graphhopper.routing.AlgorithmOptions
-import com.graphhopper.routing.util.{CarFlagEncoder, DataFlagEncoder, EncodingManager}
-import com.graphhopper.routing.weighting.{FastestWeighting, GenericWeighting, ShortestWeighting}
-import com.graphhopper.storage.index.QueryResult
+import com.graphhopper.routing.util.{DataFlagEncoder, EncodingManager}
+import com.graphhopper.routing.weighting.GenericWeighting
 import com.graphhopper.util.shapes.GHPoint
-import com.graphhopper.util.{GPXEntry, PMap, Parameters, PointList}
+import com.graphhopper.util.{GPXEntry, PMap, Parameters}
 
 import scala.collection.JavaConversions._
-import scala.collection.immutable
-import scala.util.Try
 
 /**
-  * Created by stefano on 09/10/17.
+  * @author Stefano Samele
   *
   * This object loads the graph presents in the project resources.
-  * It contains the main methods of the library.
+  * It contains two methods: one for map-matching (matchingRoute) and
+  * the other one for extend a sequence of points through routing (extendRoute)
   *
   */
-class GraphHopperMap(val graphLocation: String, val fileString: String) {
+object GraphHopperMap {
 
-  //Auxiliary constructor
-  def this(graphLocation: String) {
-    this(graphLocation, "")
-  }
-
-  //Check if the graphLocation is a directory
-  val dir: Path = Path.fromString(graphLocation)
-  if(!dir.isDirectory) throw new IllegalArgumentException("Expected a directory as graph's location")
-
-  //We create the graph object
-  val hopperOSM = new GraphHopperOSM
-
-  //Set the the elevation flag to true to include 3d dimension
-  hopperOSM.setElevation(true)
-  //We use Generic Weighting with the DataFlagEncoder
+  private var hopperOSM: GraphHopperOSM = _
   val encoder = new DataFlagEncoder
-  hopperOSM.setEncodingManager(new EncodingManager(encoder))
   val weighting = new GenericWeighting(encoder, new PMap())
-  hopperOSM.getCHFactoryDecorator.addWeighting(weighting)
-
-  //We disable the contraction hierarchies post processing. It seems to be mandatory in order to do map matching
-  hopperOSM.getCHFactoryDecorator.setEnabled(false)
-
-  if(fileString.isEmpty) {
-    //If no new map is specified, load from the resource folder
-    hopperOSM.load(graphLocation)
-  }
-  else{
-    //We assume that there already is a stored graph in the folder. We delete those files
-    val path: Path = Path.fromString(graphLocation)
-    //If false then method will throw an exception when encountering a file that cannot be deleted.  Otherwise it will continue
-    //to delete all the files that can be deleted. This method is not transactional, all files visited before failure are deleted.
-    Try(path.deleteRecursively(continueOnFailure = false))
-
-    hopperOSM.setGraphHopperLocation(graphLocation)
-    //Set the .osm file and load directly from the map
-    hopperOSM.setDataReaderFile(fileString)
-    hopperOSM.importOrLoad()
-  }
-
   val algorithm: String = Parameters.Algorithms.DIJKSTRA_BI
   val algoOptions: AlgorithmOptions = new AlgorithmOptions(algorithm, weighting)
-  val mapMatching: MapMatching = new MapMatching(hopperOSM, algoOptions)
-  mapMatching.setMeasurementErrorSigma(50)
-  //mapMatching.setMeasurementErrorSigma(20)
+
+  //Create the actual graph to be queried
+  def init(graphLocation: String): GraphHopperOSM = {
+    if (hopperOSM == null) hopperOSM = new GraphHopperOSM
+
+    //Check if the graphLocation is a directory
+    val dir: Path = Path.fromString(graphLocation)
+    if (!dir.isDirectory) throw new IllegalArgumentException("Expected a directory as graph's location")
+
+    //Set the the elevation flag to true to include 3d dimension
+    hopperOSM.setElevation(true)
+
+    //We use Generic Weighting with the DataFlagEncoder
+    hopperOSM.setEncodingManager(new EncodingManager(encoder))
+    hopperOSM.getCHFactoryDecorator.addWeighting(weighting)
+
+    //We disable the contraction hierarchies post processing. It seems to be mandatory in order to do map matching
+    hopperOSM.getCHFactoryDecorator.setEnabled(false)
+
+    //If no new map is specified, load from the resource folder
+    hopperOSM.load(graphLocation)
+    hopperOSM
+  }
 
 
   def matchingRoute(gpsPoints: java.util.List[GPXEntry]): MatchedRoute = {
+
+    val mapMatching: MapMatching = new MapMatching(hopperOSM, algoOptions)
+    mapMatching.setMeasurementErrorSigma(50)
+    //mapMatching.setMeasurementErrorSigma(20)
 
     val calcRoute: MatchResult = mapMatching.doWork(gpsPoints)
 
