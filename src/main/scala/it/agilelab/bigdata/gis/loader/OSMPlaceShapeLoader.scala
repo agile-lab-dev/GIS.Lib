c2f67abd0dc485d86b5304668283cb5b980c7ea6
Stefano_Samele <stefano.samele@agilelab.it>
2017-12-21 18:27:32 +0100

    finish osm shape loader

--- src/main/scala/it/agilelab/bigdata/gis/loader/OSMPlaceShapeLoader.scala
+++ src/main/scala/it/agilelab/bigdata/gis/loader/OSMPlaceShapeLoader.scala
@@ -8,7 +8,7 @@ import java.net.URL
 import java.io.File
 import java.util
 
-import com.vividsolutions.jts.geom.{Geometry, LineString, MultiLineString, MultiPolygon}
+import com.vividsolutions.jts.geom._
 import it.agilelab.bigdata.gis.enums.IndexType
 import it.agilelab.bigdata.gis.models._
 import it.agilelab.bigdata.gis.spatialList.GeometryList
@@ -34,20 +34,23 @@ object OSMPlaceShapeLoader{
 
 class OSMPlaceShapeLoader() extends Loader[OSMPlace]{
   override def loadFile(source: String): Iterator[(Array[AnyRef], Geometry)] = {
-    val lines: Iterator[(Array[AnyRef], MultiPolygon)] = ShapeFileReader.readMultiPolygonFeatures(source).map(e => (e._2.toArray, e._1)).toIterator
-    lines
+
+    if (source.endsWith("places_a_free_1.shp")) ShapeFileReader.readMultiPolygonFeatures(source).map(e => (e._2.toArray, e._1)).toIterator
+    else if (source.endsWith("places_free_1.shp")) ShapeFileReader.readPointFeatures(source).map(e => (e._2.toArray, e._1)).toIterator
+    else Iterator.empty
+
   }
 
   protected def objectMapping(fields: Array[AnyRef], line: Geometry): OSMPlace = {
 
-    val name: String = if(fields(5) != null) fields(3).toString else ""
-    val placetype: String = if(fields(3) != null) fields(5).toString else ""
-    val firstField: String = if(fields(1) != null) fields(1).toString  else ""
+    val name: String = if (fields(5) != null) fields(5).toString else ""
+    val placetype: String = if (fields(3) != null) fields(3).toString else ""
+    val firstField: String = if (fields(1) != null) fields(1).toString else ""
     val secondField: Int = fields(2).asInstanceOf[Int]
     val fourthField: Long = fields(4).asInstanceOf[Long]
 
-
     OSMPlace(line, name, placetype, firstField, secondField, fourthField)
+
   }
 
 
