258b37353a5a4be5692bec247da3bcf58bee8c54
Stefano_Samele <stefano.samele@agilelab.it>
2017-12-27 17:38:18 +0100

    first version reverse geocoding

--- src/main/scala/it/agilelab/bigdata/gis/loader/ShpLoaderTest.scala
+++ src/main/scala/it/agilelab/bigdata/gis/loader/ShpLoaderTest.scala
@@ -2,22 +2,19 @@ package it.agilelab.bigdata.gis.loader
 
 import java.io.{File, FilenameFilter}
 
-import com.vividsolutions.jts.algorithm.locate.IndexedPointInAreaLocator
-import com.vividsolutions.jts.geom
 import com.vividsolutions.jts.geom.{Coordinate, GeometryFactory, Point}
-import it.agilelab.bigdata.gis.models.{OSMPlace, OSMStreet}
+import it.agilelab.bigdata.gis.models.{OSMPlace, OSMStreet, partialAddress}
 import it.agilelab.bigdata.gis.spatialList.GeometryList
 import it.agilelab.bigdata.gis.spatialOperator.KNNQueryMem
-
-import scala.collection.immutable
 
 object ShpLoaderTest extends App {
 
   val path = "/home/stefano/Documents/IntesaSmartCareCore/maps"
 
-  println("[GISLib] Loading OSM file into GeometryList...")
+  println("[GISLib] Loading OSM places file into GeometryList...")
 
-  val loader  = new OSMPlaceShapeLoader()
+  val placesLoader  = new OSMPlaceShapeLoader()
+  val roadsLoader = new OSMStreetShapeLoader
   //val loader  = new OSMStreetShapeLoader()
   val folder: File = new File(path)
   val subFolders: Array[File] = folder.listFiles(
@@ -25,42 +22,68 @@ object ShpLoaderTest extends App {
     override def accept(dir: File, name: String): Boolean = name.endsWith(".shp")
     }
   )
-  val shpFiles: Array[String] = subFolders.flatMap( subFolder =>
+
+  println("[GISLib] Loading OSM places file into GeometryList...")
+
+  val placesShpFiles: Array[String] = subFolders.flatMap( subFolder =>
     subFolder.listFiles( new FilenameFilter {
       override def accept(dir: File, name: String): Boolean = name.endsWith("places_free_1.shp") || name.endsWith("places_a_free_1.shp")
     })
   ).map(_.getAbsolutePath)
 
-  val geometryList: GeometryList[OSMPlace] = loader.loadIndex(shpFiles: _*)
+  val placesGeometryList: GeometryList[OSMPlace] = placesLoader.loadIndex(placesShpFiles: _*)
 
-  println("[GISLib] Done loading OSM file into GeometryList!")
+  println("[GISLib] Done loading OSM places file into GeometryList!")
 
-  /*val latitude = 39.832223
+  println("[GISLib] Loading OSM roads file into GeometryList...")
 
-  val longitude = 18.341623*/
+  val roadsShpFiles: Array[String] = subFolders.flatMap( subFolder =>
+    subFolder.listFiles( new FilenameFilter {
+      override def accept(dir: File, name: String): Boolean = name.endsWith("roads_free_1.shp")
+    })
+  ).map(_.getAbsolutePath)
+
+  val roadsGeometryList: GeometryList[OSMStreet] = roadsLoader.loadIndex(roadsShpFiles: _*)
+
+  println("[GISLib] Done loading OSM roads file into GeometryList!")
+
+
+  val latitude = 39.832223
+
+  val longitude = 18.341623
 
   /*val latitude = 45.821003
 
   val longitude = 6.950784*/
 
-  val latitude = 45.473273
-  val longitude = 9.164086
-
+  /*val latitude = 45.473273
+  val longitude = 9.164086*/
 
 
   val fact = new GeometryFactory()
   val queryPoint: Point = fact.createPoint(new Coordinate(longitude, latitude))
 
-  val queryResult: Seq[OSMPlace] = KNNQueryMem.SpatialKnnQuery(
-    geometryList,
+  val placesResult: Seq[OSMPlace] = KNNQueryMem.SpatialKnnQuery(
+    placesGeometryList,
+    queryPoint,
+    10)
+  val placesList: Seq[OSMPlace] =  placesResult.seq
+
+  val roadsResult: Seq[OSMStreet] = KNNQueryMem.SpatialKnnQuery(
+    roadsGeometryList,
     queryPoint,
     1)
-  val list: Seq[OSMPlace] =  queryResult.seq
-  println("Results:")
-  list.foreach{
-    x =>
-      println(x.toString())
+  val roadsList: Option[OSMStreet] =  roadsResult.seq.headOption
 
-  }
+  val candidates: Seq[OSMPlace] = roadsList.map(x => {
+    placesList.filter( y => y.polygon.contains(x))
+  }).getOrElse(Seq.empty[OSMPlace])
+
+  val res: partialAddress = if (candidates.isEmpty) partialAddress(roadsList.map(_.street), placesList.headOption.map(_.name))
+  else partialAddress(roadsList.map(_.street), candidates.headOption.map(_.name))
+
+
+  println("Result of reverse-geocoding operation: " + res.toString)
+
 
 }
