5114ad5a96e3de05b4df331edcff4f697423b55e
Stefano_Samele <stefano.samele@agilelab.it>
2018-01-03 16:25:51 +0100

    Locality-and-polygon-fixes

--- src/test/java/it/agilelab/bigdata/gis/RGManagerTest.scala
+++ src/test/java/it/agilelab/bigdata/gis/RGManagerTest.scala
@@ -9,8 +9,37 @@ class RGManagerTest extends FunSuite{
 
   RGManager.init(path)
 
-  val answer = RGManager.reverseGeocode(39.832223, 18.341623)
 
-  assert(!answer.city.isEmpty && answer.street.isEmpty)
+  test("General test"){
+
+    val answer = RGManager.reverseGeocode(39.832223, 18.341623)
+
+    assert(!answer.city.isEmpty && answer.street.isEmpty)
+
+  }
+
+  test("Small polygon fine-tuning"){
+
+    val answer = RGManager.reverseGeocode(39.832931, 18.341869)
+
+    println(answer.city)
+    println(answer.street)
+
+    assert(!answer.city.isEmpty && !answer.street.isEmpty)
+
+  }
+
+  test("Filtering non useful places"){
+
+    val answer = RGManager.reverseGeocode(44.402321, 8.953425)
+
+    println(answer.city)
+    println(answer.street)
+
+    assert(!answer.city.isEmpty && !answer.street.isEmpty)
+
+
+  }
+
 
 }
