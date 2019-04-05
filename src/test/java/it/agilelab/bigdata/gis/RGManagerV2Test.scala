342045ead67f171a9dd81a3b3a1bbcf2df02b1c1
Andrea <andrea.latella87@gmail.com>
2019-04-05 18:27:24 +0200

    [#1] fix test

--- src/test/java/it/agilelab/bigdata/gis/RGManagerV2Test.scala
+++ src/test/java/it/agilelab/bigdata/gis/RGManagerV2Test.scala
@@ -16,11 +16,11 @@ class RGManagerV2Test extends FunSuite{
     println(answer.city)
     println(answer.street)
 
-    assert(!answer.city.isEmpty && !answer.street.isEmpty)
+    assert(answer.city.isDefined && answer.street.isDefined)
 
   }
 
-  test("General report validation"){
+  test("General report validation") {
 
     val points = Seq(
       (37.496343, 15.067200),
@@ -33,48 +33,31 @@ class RGManagerV2Test extends FunSuite{
 
     val result: Seq[Address] = points.map(x => ReverseGeocodingManager.reverseGeocode(x._1, x._2, true))
 
-
-    result.foreach(x => println(x))
-
     val realAddresses = Seq(
-      Address(Some("Via Colle di Frigiano"), Some("Castelsardo"), Some("Sassari"), Some("Sardegna"), Some("Italia")),
-      Address(Some("Via Roma"), Some("Pavia di Udine"), Some("Udine"), Some("Friuli Venezia Giulia"), Some("Italia")),
-      Address(Some("Via Muri Bianchi"), Some("Cittadella"), Some("Padova"), Some("Veneto"), Some("Italia") ),
-      Address(Some("Via Porta Palermo"), Some("Alcamo"), Some("Trapani"), Some("Sicilia"), Some("Italia") ),
-      Address(Some("Via 16 Aprile"), Some("Prato"), Some("Prato"), Some("Toscana"), Some("Italia") )
+      Address(Some("Lungomare Augusto Ottaviano"), Some(""), Some("Catania"), Some("Sicily"), Some("Italia")),
+      Address(Some("Largo dei Greci"), Some(""), Some("Palermo"), Some("Sicily"), Some("Italia")),
+      Address(Some("Traversa Paviglianiti"), Some(""), Some("Agrigento"), Some("Sicily"), Some("Italia")),
+      Address(None, Some(""), Some("Reggio di Calabria"), Some("Calabria"), Some("Italia")),
+      Address(Some("Piazzale dell'Aeronautica"),Some(""),Some("Lecce"),Some("Apulia"),Some("Italia")),
+      Address(Some("Via Ascanio Persio"), Some(""), Some("Matera"), Some("Basilicata"), Some("Italia"))
     )
 
+    result.zip(realAddresses).foreach(res => {
 
-//    val realAddresses = Seq(
-//      Address(Some("Via Colle di Frigiano"), Some("Castelsardo"), Some("Sassari"), Some("Sardegna"), Some("Italia")),
-//      Address(Some("Via Roma"), Some("Pavia di Udine"), Some("Udine"), Some("Friuli Venezia Giulia"), Some("Italia")),
-//      Address(Some("Via Muri Bianchi"), Some("Cittadella"), Some("Padova"), Some("Veneto"), Some("Italia") ),
-//      Address(Some("Via Porta Palermo"), Some("Alcamo"), Some("Trapani"), Some("Sicilia"), Some("Italia") ),
-//      Address(Some("Via 16 Aprile"), Some("Prato"), Some("Prato"), Some("Toscana"), Some("Italia") ),
-//      Address(Some("Via Giuseppe Garibaldi"), Some("Lecce"), Some("Lecce"), Some("Puglia"), Some("Italia") ),
-//      Address(Some("Piazza Rivoli"), Some("Torino"), Some("Torino"), Some("Piemonte"), Some("Italia") ),
-//      Address(Some("Via Casilina"), Some("Cassino"), Some("Frosinone"), Some("Lazio"), Some("Italia") ),
-//      Address(Some("Via 4 novembre"), Some("Merate"), Some("Lecco"), Some("Lombardia"), Some("Italia") ),
-//      Address(Some("Via Milano"), Some("Vittuone"), Some("Milano"), Some("Lombardia"), Some("Italia") ),
-//      Address(Some("Via Enrico de Nicola"), Some("Torre del Greco"), Some("Napoli"), Some("Campania"), Some("Italia") ),
-//      Address(Some("Via Gaetano Amati"), Some("Venaria Reale"), Some("Torino"), Some("Piemonte"), Some("Italia") ),
-//      Address(Some("Via Amerigo Vespucci"), Some("Cesano Boscone"), Some("Milano"), Some("Lombardia"), Some("Italia") ),
-//      Address(Some("Via Carlo Forlanini"), Some("Desio"), Some("Monza e della Brianza"), Some("Lombardia"), Some("Italia") ),
-//      Address(Some("Via Marino Marini"), Some("Pistoia"), Some("Pistoia"), Some("Toscana"), Some("Italia") ),
-//      Address(Some("Via per Tornavento"), Some("Lonate Pozzolo"), Some("Varese"), Some("Lombardia"), Some("Italia") ),
-//      Address(Some("Via Marchese di Villabianca"), Some("Palermo"), Some("Palermo"), Some("Sicilia"), Some("Italia") ),
-//      Address(Some("Via Nazionale"), Some("Valdidentro"), Some("Sondrio"), Some("Lombardia"), Some("Italia") ),
-//      Address(Some("Viale Porta Po"), Some("Rovigo"), Some("Rovigo"), Some("Veneto"), Some("Italia") )
-//    )
-//
-//    assert(RGAddresses.map(_.city) == realAddresses.map(_.city))
-//    assert(RGAddresses.map(_.county) == realAddresses.map(_.county))
-//    assert(RGAddresses.map(_.region) == realAddresses.map(_.region))
-//    assert(RGAddresses.map(_.country) == realAddresses.map(_.country))
-//
-//    assert(RGAddresses.map(_.street) == realAddresses.map(_.street))
+      val current = res._1
+      val expected = res._2
+
+      println("Current:  " + current)
+      println("Expected: " + expected)
+
+      assert(current.street.equals(expected.street))
+      assert(current.city.equals(expected.city))
+      assert(current.region.equals(expected.region))
+
+    })
 
   }
+
 
 
 }
