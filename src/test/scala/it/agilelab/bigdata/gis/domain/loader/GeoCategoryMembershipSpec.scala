3ca52833eb24294233a274e0b904d90ea88386c6
Andrea <andrea.latella@agilelab.it>
2020-11-01 12:31:15 +0100

    [#15] Introduce a multipoint category membership info capability
    # New features and improvements
    
    Introduce new API for check if a list of point is within a list of geometry.
    This improves performance.
    
    No Breaking change
    
    No migration
    
    No bug fixes
    
    Closes #15

--- src/test/scala/it/agilelab/bigdata/gis/domain/loader/GeoCategoryMembershipSpec.scala
+++ src/test/scala/it/agilelab/bigdata/gis/domain/loader/GeoCategoryMembershipSpec.scala
@@ -5,7 +5,11 @@ import java.util.Date
 
 import com.vividsolutions.jts.geom.{Coordinate, GeometryFactory}
 import it.agilelab.bigdata.gis.domain.managers.GeometryMembershipInfoManager
-import it.agilelab.bigdata.gis.domain.models.CategoriesCfg.{GeoMetadataCfg, Country => CountryCfg, Custom => CustomCfg}
+import it.agilelab.bigdata.gis.domain.models.CategoriesCfg.{
+  GeoMetadataCfg,
+  Country => CountryCfg,
+  Custom => CustomCfg
+}
 import it.agilelab.bigdata.gis.domain.models.{CategoriesCfg, InputCategory}
 import org.scalatest.{FlatSpec, Matchers}
 
@@ -22,25 +26,34 @@ class GeoCategoryMembershipSpec extends FlatSpec with Matchers {
 
   private val coord_toulouse = new Coordinate(1.648, 43.493)
 
-
   "Config for the GeometryMembershipManager" should "be successfully loaded" in {
     val sut = CategoriesCfg
     val result = sut.load
     result shouldBe a[Success[_]]
-    result.get.geoDataPath.toString shouldBe "src/test/resources/osm/categories"
+    result.get.geoDataPath.toString shouldBe ("src/test/resources/osm/categories")
     result.get.categoryInfo.size shouldEqual 1
     result.get.categoryInfo should contain(
-      CountryCfg(Map(2 -> GeoMetadataCfg("Code"), 3 -> GeoMetadataCfg("Name")), 3))
+      CountryCfg(
+        Map(2 -> GeoMetadataCfg("Code"), 3 -> GeoMetadataCfg("Name")),
+        3
+      )
+    )
   }
 
   "GeometryMembershipManager" should "not be initialize if categories do not match" in {
-    val aWrongPathCfg = CategoriesCfg(Paths.get("src/test/resources/osm/aPathNotExisting"), Seq(CustomCfg("aLabel", Map.empty, 0)))
+    val aWrongPathCfg = CategoriesCfg(
+      Paths.get("src/test/resources/osm/aPathNotExisting"),
+      Seq(CustomCfg("aLabel", Map.empty, 0))
+    )
     val sut = GeometryMembershipInfoManager
     sut(aWrongPathCfg) shouldBe a[Failure[_]]
   }
 
   "GeometryMembershipManager" should "load Country Category successfully" in {
-    val categoriesCfg = CategoriesCfg(Paths.get("src/test/resources/osm/categories"), Seq(CountryCfg(Map.empty, 0)))
+    val categoriesCfg = CategoriesCfg(
+      Paths.get("src/test/resources/osm/categories"),
+      Seq(CountryCfg(Map.empty, 0))
+    )
     val sut = GeometryMembershipInfoManager
     sut.apply(categoriesCfg) shouldBe a[Success[_]]
   }
@@ -51,7 +64,7 @@ class GeoCategoryMembershipSpec extends FlatSpec with Matchers {
       cfg <- CategoriesCfg.load
       sut <- GeometryMembershipInfoManager(cfg)
       point <- Try(new GeometryFactory().createPoint(coord_spain))
-    } yield sut.getGeoMembershipInfoOf(InputCategory.Country, point)
+    } yield (sut.getGeoMembershipInfoOf(InputCategory.Country, point))
 
     res shouldBe a[Success[_]]
     res.get shouldBe Seq.empty
@@ -63,7 +76,7 @@ class GeoCategoryMembershipSpec extends FlatSpec with Matchers {
       cfg <- CategoriesCfg.load
       sut <- GeometryMembershipInfoManager(cfg)
       point <- Try(new GeometryFactory().createPoint(coord_andorra))
-    } yield sut.getGeoMembershipInfoOf(InputCategory.Country, point)
+    } yield (sut.getGeoMembershipInfoOf(InputCategory.Country, point))
 
     res shouldBe a[Success[_]]
     res.get.size should be(1)
@@ -75,7 +88,9 @@ class GeoCategoryMembershipSpec extends FlatSpec with Matchers {
     val res = for {
       cfg <- CategoriesCfg.load
       sut <- GeometryMembershipInfoManager(cfg)
-      point <- Try(new GeometryFactory().createPoint(new Coordinate(18.3884, 44.1812)))
+      point <- Try(
+        new GeometryFactory().createPoint(new Coordinate(18.3884, 44.1812))
+      )
     } yield (sut.getGeoMembershipInfoOf(InputCategory.Country, point))
 
     res shouldBe a[Success[_]]
@@ -88,7 +103,9 @@ class GeoCategoryMembershipSpec extends FlatSpec with Matchers {
     val res = for {
       cfg <- CategoriesCfg.load
       sut <- GeometryMembershipInfoManager(cfg)
-      point <- Try(new GeometryFactory().createPoint(new Coordinate(7.6756, 45.0735)))
+      point <- Try(
+        new GeometryFactory().createPoint(new Coordinate(7.6756, 45.0735))
+      )
     } yield (sut.getGeoMembershipInfoOf(InputCategory.Country, point))
 
     res shouldBe a[Success[_]]
@@ -103,7 +120,9 @@ class GeoCategoryMembershipSpec extends FlatSpec with Matchers {
     val res = for {
       cfg <- CategoriesCfg.load
       sut <- GeometryMembershipInfoManager(cfg)
-      point <- Try(new GeometryFactory().createPoint(new Coordinate(1.66921, 42.50555)))
+      point <- Try(
+        new GeometryFactory().createPoint(new Coordinate(1.66921, 42.50555))
+      )
     } yield (sut.getFirstGeoMembershipInfoOf(InputCategory.Country, point))
 
     res shouldBe a[Success[_]]
@@ -115,7 +134,9 @@ class GeoCategoryMembershipSpec extends FlatSpec with Matchers {
     val res = for {
       cfg <- CategoriesCfg.load
       sut <- GeometryMembershipInfoManager(cfg)
-      point <- Try(new GeometryFactory().createPoint(new Coordinate(7.6756, 45.0735)))
+      point <- Try(
+        new GeometryFactory().createPoint(new Coordinate(7.6756, 45.0735))
+      )
     } yield (sut.getFirstGeoMembershipInfoOf(InputCategory.Country, point))
 
     res shouldBe a[Success[_]]
@@ -128,8 +149,10 @@ class GeoCategoryMembershipSpec extends FlatSpec with Matchers {
     val res = for {
       cfg <- CategoriesCfg.load
       sut <- GeometryMembershipInfoManager(cfg)
-      line <- Try(new GeometryFactory().createMultiPoint(
-        Seq(coord_spain, coord_toulouse).toArray))
+      line <- Try(
+        new GeometryFactory()
+          .createMultiPoint(Seq(coord_spain, coord_toulouse).toArray)
+      )
     } yield sut.getGeoMembershipInfoOf(InputCategory.Country, line)
 
     res shouldBe a[Success[_]]
@@ -141,13 +164,18 @@ class GeoCategoryMembershipSpec extends FlatSpec with Matchers {
     val res = for {
       cfg <- CategoriesCfg.load
       sut <- GeometryMembershipInfoManager(cfg)
-      points <- Try(new GeometryFactory().createMultiPoint(
-        Seq(coord_andorra, coord_bosnia, coord_bosnia, coord_toulouse).toArray))
+      points <- Try(
+        new GeometryFactory().createMultiPoint(
+          Seq(coord_andorra, coord_bosnia, coord_bosnia, coord_toulouse).toArray
+        )
+      )
     } yield sut.getGeoMembershipInfoOf(InputCategory.Country, points)
 
     res shouldBe a[Success[_]]
     res.get.size shouldBe 2
-    res.get.map(_.geometryLabel) should (contain("Andorra_AL2") and contain("Bosnia and Herzegovina"))
+    res.get.map(_.geometryLabel) should (contain("Andorra_AL2") and contain(
+      "Bosnia and Herzegovina"
+    ))
   }
 
   "GeometryMembershipManager.getFirstGeoMembershipInfoOf[MultiPoint]" should
@@ -155,8 +183,10 @@ class GeoCategoryMembershipSpec extends FlatSpec with Matchers {
     val res = for {
       cfg <- CategoriesCfg.load
       sut <- GeometryMembershipInfoManager(cfg)
-      line <- Try(new GeometryFactory().createMultiPoint(
-        Seq(coord_spain, coord_toulouse).toArray))
+      line <- Try(
+        new GeometryFactory()
+          .createMultiPoint(Seq(coord_spain, coord_toulouse).toArray)
+      )
     } yield sut.getGeoMembershipInfoOf(InputCategory.Country, line)
 
     res shouldBe a[Success[_]]
@@ -168,13 +198,18 @@ class GeoCategoryMembershipSpec extends FlatSpec with Matchers {
     val res = for {
       cfg <- CategoriesCfg.load
       sut <- GeometryMembershipInfoManager(cfg)
-      points <- Try(new GeometryFactory().createMultiPoint(
-        Seq(coord_andorra, coord_bosnia, coord_toulouse).toArray))
+      points <- Try(
+        new GeometryFactory().createMultiPoint(
+          Seq(coord_andorra, coord_bosnia, coord_toulouse).toArray
+        )
+      )
     } yield sut.getGeoMembershipInfoOf(InputCategory.Country, points)
 
     res shouldBe a[Success[_]]
     res.get.size shouldBe 2
-    res.get.map(_.geometryLabel) should (contain("Andorra_AL2") or contain("Bosnia and Herzegovina"))
+    res.get.map(_.geometryLabel) should (contain("Andorra_AL2") or contain(
+      "Bosnia and Herzegovina"
+    ))
   }
 
   "GeometryMembershipManager.getFirstGeoMembershipInfoOf[MultiPoint] - STRICT" should
@@ -182,9 +217,17 @@ class GeoCategoryMembershipSpec extends FlatSpec with Matchers {
     val res = for {
       cfg <- CategoriesCfg.load
       sut <- GeometryMembershipInfoManager(cfg)
-      points <- Try(new GeometryFactory().createMultiPoint(
-        Seq(coord_andorra, coord_bosnia, coord_toulouse).toArray))
-    } yield sut.getGeoMembershipInfoOf(InputCategory.Country, points, strictMembership = true)
+      points <- Try(
+        new GeometryFactory().createMultiPoint(
+          Seq(coord_andorra, coord_bosnia, coord_toulouse).toArray
+        )
+      )
+    } yield
+      sut.getGeoMembershipInfoOf(
+        InputCategory.Country,
+        points,
+        strictMembership = true
+      )
 
     res shouldBe a[Success[_]]
     res.get.size shouldBe 0
@@ -195,15 +238,23 @@ class GeoCategoryMembershipSpec extends FlatSpec with Matchers {
     val res = for {
       cfg <- CategoriesCfg.load
       sut <- GeometryMembershipInfoManager(cfg)
-      points <- Try(new GeometryFactory().createMultiPoint(
-        Seq(coord_andorra, coord_bosnia).toArray))
-    } yield sut.getGeoMembershipInfoOf(InputCategory.Country, points, strictMembership = true)
+      points <- Try(
+        new GeometryFactory()
+          .createMultiPoint(Seq(coord_andorra, coord_bosnia).toArray)
+      )
+    } yield
+      sut.getGeoMembershipInfoOf(
+        InputCategory.Country,
+        points,
+        strictMembership = true
+      )
 
     res shouldBe a[Success[_]]
     res.get.size shouldBe 2
-    res.get.map(_.geometryLabel) should (contain("Andorra_AL2") and contain("Bosnia and Herzegovina"))
+    res.get.map(_.geometryLabel) should (contain("Andorra_AL2") and contain(
+      "Bosnia and Herzegovina"
+    ))
   }
-
 
   "GeometryMembershipManager.getGeoMembershipInfoOf[MultiPoint]" should
     "performs well while benchmarking" in {
@@ -211,31 +262,42 @@ class GeoCategoryMembershipSpec extends FlatSpec with Matchers {
     val res = for {
       cfg <- CategoriesCfg.load
       sut <- GeometryMembershipInfoManager(cfg)
-      seq = (1 to 1) flatMap (_ => List(
-        coord_andorra,
-        coord_andorra,
-        coord_andorra,
-        coord_spain,
-        coord_andorra,
-        coord_bosnia,
-        coord_bosnia,
-        coord_bosnia,
-        coord_bosnia,
-        coord_bosnia,
-        coord_bosnia,
-        coord_bosnia,
-        coord_bosnia,
-        coord_bosnia,
-        coord_bosnia,
-        coord_bosnia))
+      seq = (1 to 1) flatMap (
+        _ =>
+          List(
+            coord_andorra,
+            coord_andorra,
+            coord_andorra,
+            coord_spain,
+            coord_andorra,
+            coord_bosnia,
+            coord_bosnia,
+            coord_bosnia,
+            coord_bosnia,
+            coord_bosnia,
+            coord_bosnia,
+            coord_bosnia,
+            coord_bosnia,
+            coord_bosnia,
+            coord_bosnia,
+            coord_bosnia
+          )
+      )
       point <- Try(new GeometryFactory().createMultiPoint(seq.toArray))
       runnable = new Runnable {
-        override def run(): Unit = sut.getGeoMembershipInfoOf(InputCategory.Country, point, strictMembership = true)
+        override def run(): Unit =
+          sut.getGeoMembershipInfoOf(
+            InputCategory.Country,
+            point,
+            strictMembership = true
+          )
       }
     } yield runnable
 
     val value = res.get
-    val threads = (1 to 4).map(_ => new Thread(value)) ++ (1 to 4).map(_ => new Thread(value))
+    val threads = (1 to 4).map(_ => new Thread(value)) ++ (1 to 4).map(
+      _ => new Thread(value)
+    )
     val t0 = System.currentTimeMillis()
     println(new Date().toInstant)
     threads.foreach(_.start())
@@ -245,6 +307,5 @@ class GeoCategoryMembershipSpec extends FlatSpec with Matchers {
     println(t0 - t1 abs)
 
   }
-
 
 }
