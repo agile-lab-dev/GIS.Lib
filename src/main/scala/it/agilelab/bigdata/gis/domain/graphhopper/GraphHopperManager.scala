b0009161b29356e3e067c8e70fc1f0afdc60ca08
Pierangelo Di Pilato <pierangelo.dipilato@agilelab.it>
2021-03-02 09:08:00 +0100

    Handler error in matching route
    - Remove getMatchingRoute since it became useless.
    
    Signed-off-by: Pierangelo Di Pilato <pierangelo.dipilato@agilelab.it>

--- src/main/scala/it/agilelab/bigdata/gis/domain/graphhopper/GraphHopperManager.scala
+++ src/main/scala/it/agilelab/bigdata/gis/domain/graphhopper/GraphHopperManager.scala
@@ -1,6 +1,6 @@
 package it.agilelab.bigdata.gis.domain.graphhopper
 
-import com.graphhopper.matching.{EdgeMatch, GPXExtension, MatchResult}
+import com.graphhopper.matching.{EdgeMatch, GPXExtension}
 import com.graphhopper.util.details.PathDetailsBuilderFactory
 import com.typesafe.config.Config
 import it.agilelab.bigdata.gis.core.utils.Logger
@@ -9,7 +9,6 @@ import it.agilelab.bigdata.gis.domain.loader.RouteMatcher
 
 import java.util
 import scala.collection.JavaConversions._
-import scala.util.{Failure, Success, Try}
 
 case class GraphHopperManager(conf: Config) extends RouteMatcher with Logger {
 
@@ -20,143 +19,138 @@ case class GraphHopperManager(conf: Config) extends RouteMatcher with Logger {
       //TODO is this right?
       .getOrElse("unclassified")
 
-  @throws(classOf[RuntimeException])
-  @throws(classOf[IllegalAccessException])
-  private def getMatchingRoute(gpsPoints: Seq[GPSPoint]) = {
-    val calcRoute = graphConf.mapMatching.doWork(gpsPoints.map(_.toGPXEntry))
+  override def matchingRoute(gpsPoints: Seq[GPSPoint]): Either[MatchedRouteError, MatchedRoute] = {
+    try {
+      val calcRoute = graphConf.mapMatching.doWork(gpsPoints.map(_.toGPXEntry))
 
-    val length = calcRoute.getMatchLength
-    val time = calcRoute.getMatchMillis
-    val edges: Seq[EdgeMatch] = calcRoute.getEdgeMatches
+      val length = calcRoute.getMatchLength
+      val time = calcRoute.getMatchMillis
+      val edges: Seq[EdgeMatch] = calcRoute.getEdgeMatches
 
-    if (calcRoute.getMergedPath.calcEdges().nonEmpty) {
+      if (calcRoute.getMergedPath.calcEdges().nonEmpty) {
 
-      val mappedEdges: Seq[(String, Double)] =
-        edges
-          .map(edge => (graphConf.encoder.getHighwayAsString(edge.getEdgeState), edge.getEdgeState.getDistance))
-          .map {
-            case (null, value) => ("unclassified", value)
-            case x: (String, Double) => x
-          }
+        val mappedEdges: Seq[(String, Double)] =
+          edges
+            .map(edge => (graphConf.encoder.getHighwayAsString(edge.getEdgeState), edge.getEdgeState.getDistance))
+            .map {
+              case (null, value) => ("unclassified", value)
+              case x: (String, Double) => x
+            }
 
-      //retrieve distance for each pair of points
-      val distances = calcRoute.getMergedPath
-        .calcDetails(List("distance"), new PathDetailsBuilderFactory, 0)
-        .get("distance")
+        //retrieve distance for each pair of points
+        val distances = calcRoute.getMergedPath
+          .calcDetails(List("distance"), new PathDetailsBuilderFactory, 0)
+          .get("distance")
 
-      //retrieve all edges
-      val allEdges: Seq[EnrichEdge] = edges.toList.flatMap(edge => {
-        val gpsExtensions: util.List[GPXExtension] = edge.getGpxExtensions
-        val edgeId = edge.getEdgeState.getBaseNode
+        //retrieve all edges
+        val allEdges: Seq[EnrichEdge] = edges.toList.flatMap(edge => {
+          val gpsExtensions: util.List[GPXExtension] = edge.getGpxExtensions
+          val edgeId = edge.getEdgeState.getBaseNode
 
-        if (gpsExtensions.size() == 0)
-          List(
-            EnrichEdge(edgeId, isInitialNode = false, typeOfRoute(edge), None)
-          )
-        else
-          gpsExtensions.flatMap(item => {
+          if (gpsExtensions.size() == 0)
             List(
-              EnrichEdge(
-                item.getQueryResult.getClosestNode,
-                isInitialNode = true,
-                typeOfRoute(edge),
-                Some(
-                  TracePoint(
-                    item.getEntry.lat,
-                    item.getEntry.lon,
-                    Some(item.getEntry.ele),
-                    item.getEntry.getTime,
-                    Some(item.getQueryResult.getSnappedPoint.lat),
-                    Some(item.getQueryResult.getSnappedPoint.lon),
-                    Some(item.getQueryResult.getSnappedPoint.ele),
-                    Some(typeOfRoute(edge)),
-                    Some(edge.getEdgeState.getName),
-                    Some(graphConf.encoder.getSpeed(item.getQueryResult.getClosestEdge.getFlags).toInt),
-                    Some(item.getQueryResult.getQueryDistance)
+              EnrichEdge(edgeId, isInitialNode = false, typeOfRoute(edge), None)
+            )
+          else
+            gpsExtensions.flatMap(item => {
+              List(
+                EnrichEdge(
+                  item.getQueryResult.getClosestNode,
+                  isInitialNode = true,
+                  typeOfRoute(edge),
+                  Some(
+                    TracePoint(
+                      item.getEntry.lat,
+                      item.getEntry.lon,
+                      Some(item.getEntry.ele),
+                      item.getEntry.getTime,
+                      Some(item.getQueryResult.getSnappedPoint.lat),
+                      Some(item.getQueryResult.getSnappedPoint.lon),
+                      Some(item.getQueryResult.getSnappedPoint.ele),
+                      Some(typeOfRoute(edge)),
+                      Some(edge.getEdgeState.getName),
+                      Some(graphConf.encoder.getSpeed(item.getQueryResult.getClosestEdge.getFlags).toInt),
+                      Some(item.getQueryResult.getQueryDistance)
+                    )
                   )
                 )
               )
-            )
-          })
+            })
 
-      })
+        })
 
-      //union information on edge with information on distances
-      val distancesBetweenEdge: Seq[EnrichEdgeWithDistance] =
-        allEdges.zip(distances).map {
-          case (edge, distance) =>
-            EnrichEdgeWithDistance(
-              edge.idNode,
-              edge.isInitialNode,
-              edge.typeOfRoute,
-              edge.node,
-              distance.getValue.asInstanceOf[Double]
-            )
-        }
-
-      //retrieve index of only initialNode
-      val idxInitialNode: Seq[Int] =
-        distancesBetweenEdge.zipWithIndex
-          .filter {
-            case (edgeWithDistance, idx) => edgeWithDistance.isInitialNode
-          }
-          .map {
-            case (edgeWithDistance, idx) => idx
-          }
-
-      // distances between node
-      val distancesBetweenNode =
-        idxInitialNode
-          .zip(idxInitialNode.tail)
-          .map {
-            case (fromIdx, toIdx) =>
-              val subList = distancesBetweenEdge.subList(fromIdx, toIdx + 1)
-
-              val (typesOfRoute, nrOccurrences) =
-                subList
-                  .map(_.typeOfRoute)
-                  .groupBy(identity)
-                  .mapValues(_.size)
-                  .reduce((a, b) => if (a._2 > b._2) a else b)
-
-              val distance =
-                subList
-                  .foldLeft(0D)((acc, elem) => {
-                    acc + elem.distance
-                  })
-
-              DistancePoint(
-                subList.head.node.get,
-                subList.last.node.get,
-                distance,
-                subList.last.node.get.time - subList.head.node.get.time,
-                typesOfRoute
+        //union information on edge with information on distances
+        val distancesBetweenEdge: Seq[EnrichEdgeWithDistance] =
+          allEdges.zip(distances).map {
+            case (edge, distance) =>
+              EnrichEdgeWithDistance(
+                edge.idNode,
+                edge.isInitialNode,
+                edge.typeOfRoute,
+                edge.node,
+                distance.getValue.asInstanceOf[Double]
               )
           }
 
-      val routeTypesKm: Map[String, Double] =
-        mappedEdges
-          .groupBy(_._1)
-          .map(x => (x._1, x._2.map(_._2).sum))
+        //retrieve index of only initialNode
+        val idxInitialNode: Seq[Int] =
+          distancesBetweenEdge.zipWithIndex
+            .filter {
+              case (edgeWithDistance, idx) => edgeWithDistance.isInitialNode
+            }
+            .map {
+              case (edgeWithDistance, idx) => idx
+            }
 
-      val points: Seq[TracePoint] =
-        allEdges.filter(_.isInitialNode).map(_.node.get)
+        // distances between node
+        val distancesBetweenNode =
+          idxInitialNode
+            .zip(idxInitialNode.tail)
+            .map {
+              case (fromIdx, toIdx) =>
+                val subList = distancesBetweenEdge.subList(fromIdx, toIdx + 1)
 
-      MatchedRoute(
-        points,
-        length,
-        time,
-        routeTypesKm,
-        distancesBetweenNode
-      )
-    } else
-        MatchedRoute(gpsPoints.map(_.toTracePoint), length, time, Map.empty, Seq.empty)
-  }
+                val (typesOfRoute, nrOccurrences) =
+                  subList
+                    .map(_.typeOfRoute)
+                    .groupBy(identity)
+                    .mapValues(_.size)
+                    .reduce((a, b) => if (a._2 > b._2) a else b)
 
-  override def matchingRoute(gpsPoints: Seq[GPSPoint]): Either[MatchedRouteError, MatchedRoute] = {
-    Try(getMatchingRoute(gpsPoints)) match {
-      case Success(matchingRoute) => Right(matchingRoute)
-      case Failure(ex) => Left(MatchedRouteError(ex))
+                val distance =
+                  subList
+                    .foldLeft(0D)((acc, elem) => {
+                      acc + elem.distance
+                    })
+
+                DistancePoint(
+                  subList.head.node.get,
+                  subList.last.node.get,
+                  distance,
+                  subList.last.node.get.time - subList.head.node.get.time,
+                  typesOfRoute
+                )
+            }
+
+        val routeTypesKm: Map[String, Double] =
+          mappedEdges
+            .groupBy(_._1)
+            .map(x => (x._1, x._2.map(_._2).sum))
+
+        val points: Seq[TracePoint] =
+          allEdges.filter(_.isInitialNode).map(_.node.get)
+
+        Right(MatchedRoute(
+          points,
+          length,
+          time,
+          routeTypesKm,
+          distancesBetweenNode
+        ))
+      } else
+          Right(MatchedRoute(gpsPoints.map(_.toTracePoint), length, time, Map.empty, Seq.empty))
+    } catch {
+      case ex: _ => Left(MatchedRouteError(ex))
     }
   }
 }
