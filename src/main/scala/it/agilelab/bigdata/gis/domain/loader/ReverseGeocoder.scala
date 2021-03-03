82ba3a6745b31b11d93b2a38d792bf2762af5b25
Pierangelo Di Pilato <pierangelo.dipilato@agilelab.it>
2021-03-03 10:42:58 +0100

    [#47] Add id to ReverseGeocodingResponse and GPSPoint
    - Add id to `ReverseGeocodingResponse`
    - Add `IdentifiableGPSPoint` class which extends GPSPoint and adds an
      id to it.
    
    Closes #47
    
    Signed-off-by: Pierangelo Di Pilato <pierangelo.dipilato@agilelab.it>

--- src/main/scala/it/agilelab/bigdata/gis/domain/loader/ReverseGeocoder.scala
+++ src/main/scala/it/agilelab/bigdata/gis/domain/loader/ReverseGeocoder.scala
@@ -1,11 +1,11 @@
 package it.agilelab.bigdata.gis.domain.loader
 
 import it.agilelab.bigdata.gis.domain.exceptions.ReverseGeocodingError
-import it.agilelab.bigdata.gis.domain.graphhopper.GPSPoint
+import it.agilelab.bigdata.gis.domain.graphhopper.IdentifiableGPSPoint
 import it.agilelab.bigdata.gis.domain.models.ReverseGeocodingResponse
 
 trait ReverseGeocoder {
 
-  def reverseGeocode(point: GPSPoint) : Either[ReverseGeocodingError, ReverseGeocodingResponse]
+  def reverseGeocode(point: IdentifiableGPSPoint) : Either[ReverseGeocodingError, ReverseGeocodingResponse]
 
 }
