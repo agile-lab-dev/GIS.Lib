1c2f693d1119d311aba1f00011eac3a9af0a63e5
Marco Arena <marco.arena@agilelab.it>
2021-10-04 11:21:23 +0200

    [#107] Circle computes distance with harvesine formula

--- src/test/scala/it/agilelab/gis/core/GeometryDistanceSpec.scala
+++ src/test/scala/it/agilelab/gis/core/GeometryDistanceSpec.scala
@@ -5,18 +5,18 @@ import org.scalatest.{ FlatSpec, Matchers }
 
 class GeometryDistanceSpec extends FlatSpec with Matchers {
   "Circle distance method" should "return the correct distance from a point" in {
-    val wktStringCircle: String = "CIRCLE((100 100),50)"
-    val wktStringPoint: String = "POINT(0 100)"
+    val wktStringCircle: String = "CIRCLE ((11.028014 45.33984), 200)"
+    val wktStringPoint: String = "POINT(11.002371648547008 45.3416411637726)"
     val circle = WktConverter.converter(wktStringCircle)
     val point = WktConverter.converter(wktStringPoint)
-    val expected = 50d
-    circle.right.get.distance(point.right.get) shouldEqual expected
+    val expected = 1814
+    circle.right.get.distance(point.right.get).toInt shouldEqual expected
 
   }
 
   it should "return zero distance from a point on the border" in {
-    val wktStringCircle: String = "CIRCLE((100 100),50)"
-    val wktStringPoint = "POINT(50 100)"
+    val wktStringCircle: String = "CIRCLE ((11.028014 45.33984), 200)"
+    val wktStringPoint = "POINT(11.030031109783607 45.34094093409899)"
     val circle = WktConverter.converter(wktStringCircle)
     val point = WktConverter.converter(wktStringPoint)
     val expected = 0d
@@ -25,8 +25,8 @@ class GeometryDistanceSpec extends FlatSpec with Matchers {
   }
 
   it should "return zero distance from a point because internal" in {
-    val wktStringCircle: String = "CIRCLE((100 100),50)"
-    val wktStringPoint: String = "POINT(70 100)"
+    val wktStringCircle: String = "CIRCLE((11.028014 45.33984), 200)"
+    val wktStringPoint: String = "POINT(11.028797238084936 45.34044324524726)"
     val circle = WktConverter.converter(wktStringCircle)
     val point = WktConverter.converter(wktStringPoint)
     val expected = 0d
