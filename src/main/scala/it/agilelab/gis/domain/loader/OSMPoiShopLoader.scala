242d9cf99f360051ab389b7de6cc32a4c711ff33
Cristian Astorino <cristian.astorino@agilelab.it>
2022-07-11 15:49:09 +0200

    [#130] Read shape file attributes from specific columns # New features and improvements Attributes for points of interest are now read from dedicated columns of the shape file
    # Related issue
    Closes #130

--- src/main/scala/it/agilelab/gis/domain/loader/OSMPoiShopLoader.scala
+++ src/main/scala/it/agilelab/gis/domain/loader/OSMPoiShopLoader.scala
@@ -13,17 +13,13 @@ class OSMPoiShopLoader extends OSMPoiLoader[OSMPoiShop] {
   override protected def objectMapping(fields: Array[AnyRef], geometry: Geometry): OSMPoiShop = {
     val osmId = Try(Option(fields(1)).map(_.toString).getOrElse("")).toOption
     geometry match {
-      case _: MultiLineString =>
+      case _: MultiLineString | _: Point =>
         val name = Try(Option(fields(2)).map(_.toString).getOrElse("")).toOption
-        val shop = getInfosFromOtherTags(fields, 9, "shop")
+        val shop = Try(Option(fields(7)).map(_.toString).getOrElse("")).toOption
         OSMPoiShop(geometry, osmId, name = name, shop = shop)
       case _: MultiPolygon =>
         val name = Try(Option(fields(3)).map(_.toString).getOrElse("")).toOption
-        val shop = Try(Option(fields(22)).map(_.toString).getOrElse("")).toOption
-        OSMPoiShop(geometry, osmId, name = name, shop = shop)
-      case _: Point =>
-        val name = Try(Option(fields(2)).map(_.toString).getOrElse("")).toOption
-        val shop = getInfosFromOtherTags(fields, 10, "shop")
+        val shop = Try(Option(fields(8)).map(_.toString).getOrElse("")).toOption
         OSMPoiShop(geometry, osmId, name = name, shop = shop)
       case _ =>
         OSMPoiShop(geometry, osmId)
